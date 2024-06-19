287be1b8d666546629630997364172e42da7d195
Lene Christensen <lene.hojberg.christensen@gmail.com>
2024-06-19 18:14:10 +0200

    Updated defs.vhd

--- accelerator/defs.vhd
+++ accelerator/defs.vhd
@@ -33,9 +33,11 @@ use work.types.all;
 --use UNISIM.VComponents.all;
 
 package defs is
-    constant MAX_STACK : Integer := 256;
+    constant INPUT_SIZE : integer := 4;
     constant INSTRUCTIONS : array_type(4 downto 0)(18 downto 0) := ("0000000000000010001", "0000000000000010010", "0000000000000010001", "0000000000000010010", "0000000000000010000");
-    constant INPUT_SIZE : Integer := 4;
+    constant DATA_WIDTH : integer := 16;
+    constant INTEGER_WIDTH : integer := 1;
+    constant DECIMAL_WIDTH : integer := DATA_WIDTH - INTEGER_WIDTH;
     constant INST_SIZE : Integer := 3;
 
 end defs;
