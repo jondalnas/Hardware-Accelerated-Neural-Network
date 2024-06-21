5c235a389fc8cad923240844417d65dd4d92cfae
Lene Christensen <lene.hojberg.christensen@gmail.com>
2024-06-21 19:09:05 +0200

    Updated and tested conv to use FSM and reduce generated hardware

--- accelerator/conv.vhd
+++ accelerator/conv.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.MATH_REAL.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 use work.types.all;
 
@@ -19,6 +20,10 @@ entity conv is
         y_size : integer
     );
     Port (
+        clk : in std_logic;
+        rst : in std_logic;
+        valid_in : in std_logic;
+        valid_out : out std_logic;
         x : in array_type(x_size - 1 downto 0)(data_width - 1 downto 0);
         w : in array_type(w_size - 1 downto 0)(data_width - 1 downto 0);
         y : out array_type(y_size - 1 downto 0)(data_width - 1 downto 0)
@@ -31,6 +36,22 @@ architecture Behavioral of conv is
 
 	constant x_offs : integer := integer(ceil(real(kernel_shape(0) * dilation(0) - (dilation(0) - 1)) / 2)) - 1;
 	constant y_offs : integer := integer(ceil(real(kernel_shape(1) * dilation(1) - (dilation(1) - 1)) / 2)) - 1;
+	
+	type state_type is (START, DONE, SUM, MULT);
+    signal state, next_state : state_type;
+    
+    signal mult_a : res_array;
+    signal mult_b : res_array;
+    signal mult_in_a : signed(data_width - 1 downto 0);
+    signal mult_in_b : signed(data_width - 1 downto 0);
+    signal mult_out : signed(data_width - 1 downto 0);
+    signal sum_in : array_type(kernel_size - 1 downto 0)(data_width - 1 downto 0);
+    signal sum_out : signed(data_width - 1 downto 0);
+    
+    signal next_out_array, out_array : array_type(y_size - 1 downto 0)(data_width - 1 downto 0);
+    
+    signal next_index, index : integer;
+    signal next_index_sum, index_sum : integer;
 begin
 	dim2 : if num_dimensions = 4 generate
 		--Assuming no layers to input image, might be wrong
@@ -47,33 +68,112 @@ begin
 							xk : for xxk in 0 to kernel_shape(0) - 1 generate
 								-- If OOB, set element to 0
 								oob : if (yy * stride(1) + yyk * dilation(1) - y_offs < 0) or (xx * stride(0) + xxk * dilation(0) - x_offs < 0) or (yy * stride(1) + yyk * dilation(1) - y_offs >= dimensions_x(1)) or (xx * stride(0) + xxk * dilation(0) - x_offs >= dimensions_x(0)) generate
-									res(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0)) <= (others => '0');
+									mult_a(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0)) <= (others => '0');
+									mult_b(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0)) <= (others => '0');
 								else generate
-									mul : entity work.fix_mul
-										generic map(
-											data_width => 16
-										)
-										port map(
-											a => x((xx * stride(0) + xxk * dilation(0) - x_offs) + (yy * stride(1) + yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)),
-											b => w(xxk + yyk * dimensions_w(0) + cc * dimensions_w(0) * dimensions_w(1) + llk * dimensions_w(0) * dimensions_w(1) * dimensions_w(2)),
-											res => res(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1))
-										);
+								    mult_a(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x((xx * stride(0) + xxk * dilation(0) - x_offs) + (yy * stride(1) + yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1));
+								    mult_b(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= w(xxk + yyk * dimensions_w(0) + cc * dimensions_w(0) * dimensions_w(1) + llk * dimensions_w(0) * dimensions_w(1) * dimensions_w(2));
 								end generate;
 							end generate;
-						end generate;
-
-						sum : entity work.sum
-							generic map(
-								data_width => data_width,
-								num_inputs => kernel_size
-							)
-							port map(
-								a => res(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1)),
-								c => y(xx + yy * dimensions_x(0) + llk * dimensions_x(0) * dimensions_x(1))
-							);
+						end generate;	
 					end generate;
 				end generate;
 			end generate;
 		end generate;
 	end generate;
+	
+	 process(all)
+    begin
+        valid_out <= '0';
+        next_out_array <= out_array;
+        sum_in <= (others => (others => '0'));
+        mult_in_a <= (others => '0');
+        mult_in_b <= (others => '0');
+        next_index <= 0;
+        next_index_sum <= 0;
+        next_state <= state;
+        
+        case state is
+            when START =>
+                next_index <= 0;
+                next_index_sum <= 0;
+                valid_out <= '0';
+                if valid_in then
+                    next_state <= MULT;
+                else 
+                    next_state <= START;
+                end if;
+            
+            when MULT => 
+                next_index <= index + 1;
+                next_index_sum <= index_sum;
+                mult_in_a <= mult_a(index_sum)(index);
+                mult_in_b <= mult_b(index_sum)(index);
+                res(index_sum)(index) <= mult_out;
+                if index = kernel_size - 1 then
+                    next_state <= SUM;
+                else
+                    next_state <= MULT;
+                end if;
+            when SUM => 
+                next_index_sum <= index_sum + 1;
+                next_index <= 0;
+                sum_in <= res(index_sum);
+                next_out_array(index_sum) <= sum_out;
+                if index_sum = y_size - 1 then
+                    next_state <= DONE;
+                else
+                    next_state <= MULT;
+                end if;
+            when DONE =>
+                next_index <= 0;
+                valid_out <= '1';
+                if not valid_in then
+                    next_state <= START;
+                else
+                    next_state <= DONE;
+                end if;
+        end case;
+    
+    end process;
+    
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst then
+                state <= START;
+                index <= 0;
+                index_sum <= 0;
+                out_array <= (others => (others => '0'));
+            else
+                state <= next_state;
+                index <= next_index;
+                index_sum <= next_index_sum;
+                out_array <= next_out_array;
+            end if;
+        end if;
+    end process;
+    
+    y <= out_array;
+    
+    mul : entity work.fix_mul
+        generic map(
+            data_width => 16
+        )
+        port map(
+            a => mult_in_a,
+            b => mult_in_b,
+            res => mult_out
+        );
+        
+    sum1 : entity work.sum
+        generic map(
+            data_width => data_width,
+            num_inputs => kernel_size
+        )
+        port map(
+            a => sum_in,
+            c => sum_out
+        );
+    
 end Behavioral;
--- accelerator/test/conv_tb.vhd
+++ accelerator/test/conv_tb.vhd
@@ -13,26 +13,33 @@ architecture tb of conv_tb is
 	signal x : array_type(8 downto 0)(15 downto 0);
 	signal w : array_type(17 downto 0)(15 downto 0);
 	signal y : array_type(17 downto 0)(15 downto 0);
+	signal clk, rst, valid_in, valid_out : std_logic := '0';
 begin
+    clk <= not clk after 500 ns;
+
 	dut : entity work.conv
-		generic map(
-			data_width => 16,
-			num_dimensions => 4,
-			x_size => 9,
-			w_size => 18,
-			y_size => 18,
-			kernel_size => 9,
-			dimensions_x => (1, 1, 3, 3),
-			dimensions_w => (2, 1, 3, 3),
-			kernel_shape => (3, 3),
-			dilation => (2, 2),
-			stride => (1, 1)
-		)
-		port map(
-			x => x,
-			w => w,
-			y => y
-		);
+        generic map(
+            data_width => 16,
+            num_dimensions => 4,
+            x_size => 9,
+            w_size => 18,
+            y_size => 18,
+            kernel_size => 9,
+            dimensions_x => (1, 1, 3, 3),
+            dimensions_w => (2, 1, 3, 3),
+            kernel_shape => (3, 3),
+            dilation => (2, 2),
+            stride => (1, 1)
+        )
+        port map(
+            clk => clk,
+            rst => rst,
+            valid_in => valid_in,
+            valid_out => valid_out,
+            x => x,
+            w => w,
+            y => y
+        );
 	
 	process
         variable seed1, seed2 : integer := 999;
@@ -52,6 +59,11 @@ begin
             return slv;
         end function;
 	begin
+	   rst <= '1';
+	   wait for 500 ns;
+	   rst <= '0';
+	   wait for 500 ns;
+	   
 		x <= ("0100000000000000", "0100000000000000", "0100000000000000",
 		      "0100000000000000", "0100000000000000", "0100000000000000",
 		      "0100000000000000", "0100000000000000", "0100000000000000");
@@ -64,6 +76,8 @@ begin
 		      "0001100000000000", "0000011000000000", "0001100000000000",
 		      "0001000000000000", "0001100000000000", "0001000000000000");
 
-		wait for 1 ns;
+        valid_in <= '1';
+		wait until valid_out = '1';
+		valid_in <= '0';
 	end process;
 end tb;
