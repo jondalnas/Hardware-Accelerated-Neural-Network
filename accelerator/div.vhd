d477ef101bc9de7dba455f551b97be1e37f4d750
Jonas Jensen <jdudal11@gmail.com>
2024-06-21 19:07:25 +0200

    Converted div to FSM and updated testbench

--- accelerator/div.vhd
+++ accelerator/div.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 use work.types.all;
 
@@ -20,19 +21,78 @@ entity div is
 end div;
 
 architecture Behavioral of div is
+	type state_type is (START, DONE, CALC);
+	signal state, next_state : state_type;
 
+	signal dividend, divisor, div_res : signed(data_width - 1 downto 0);
+
+	signal res, next_res : array_type(input_size - 1 downto 0)(data_width - 1 downto 0);
+
+	signal index, next_index : integer := 0;
 begin
 
-    arr_div_gen : for i in 0 to input_size - 1 generate
-        div: entity work.fix_div
-        generic map (
-            data_width => data_width
-        )
-        port map (
-            dividend => a(i),
-            divisor => b(i),
-            res => c(i)
-        );
-    end generate;
+	process(all)
+	begin
+		valid_out <= '0';
+
+		next_state <= state;
+		dividend <= (others => '0');
+		divisor <= (others => '0');
+		next_res <= res;
+		next_index <= 0;
+
+		case state is
+			when START =>
+				next_state <= START;
+				if valid_in then
+					next_state <= CALC;
+				end if;
+
+			when CALC =>
+				next_state <= CALC;
+				next_index <= index + 1;
+
+				next_res(index) <= div_res;
+
+				if index = input_size - 1 then
+					next_state <= DONE;
+				    next_index <= 0;
+				end if;
+
+			when DONE =>
+				next_state <= DONE;
+				valid_out <= '1';
+				if not valid_in then
+					next_state <= START;
+				end if;
+		end case;
+	end process;
+
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if rst then
+				state <= START;
+				res <= (others => (others => '0'));
+				index <= 0;
+			else
+				state <= next_state;
+				res <= next_res;
+				index <= next_index;
+			end if;
+		end if;
+	end process;
+
+	c <= res;
+
+	div: entity work.fix_div
+	generic map (
+		data_width => data_width
+	)
+	port map (
+		dividend => a(index),
+		divisor => b(index),
+		res => div_res
+	);
 
 end Behavioral;
--- accelerator/test/div_tb.vhd
+++ accelerator/test/div_tb.vhd
@@ -11,11 +11,11 @@ entity div_tb is
 end div_tb;
 
 architecture div_test of div_tb is
-
     signal a, b, c : array_type(5 downto 0)(DATA_WIDTH - 1 downto 0);
     
-
+	signal clk, rst, valid_in, valid_out : std_logic := '0';
 begin
+	clk <= not clk after 100 ns;
 
     div : entity work.div
         generic map(
@@ -23,14 +23,17 @@ begin
             data_width => DATA_WIDTH
         )
         port map (
+			clk => clk,
+			rst => rst,
+			valid_in => valid_in,
+			valid_out => valid_out,
             a => a,
             b => b,
             c => c
         );
         
         
-    process
-    
+    process    
         variable seed1, seed2 : integer := 999;
         
         impure function rand_slv(len : integer) return std_logic_vector is
@@ -48,11 +51,19 @@ begin
             return slv;
         end function;
     begin
+		rst <= '1';
+		wait for 200 ns;
+		rst <= '0';
+
         for i in 0 to 5 loop
             a(i) <= signed(rand_slv(DATA_WIDTH));
             b(i) <= signed(rand_slv(DATA_WIDTH));
         end loop;
+		valid_in <= '1';
         
-        wait for 200 ns;
+        wait until valid_out = '1';
+		valid_in <= '0';
+
+		wait until valid_out = '0';
     end process;
 end div_test;
