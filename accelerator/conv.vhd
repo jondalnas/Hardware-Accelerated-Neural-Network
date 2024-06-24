13d0d0d19b701e64f68ab255891132cba0c37122
Jonas Jensen <jdudal11@gmail.com>
2024-06-24 12:48:46 +0200

    Updated convolution to use less wires

--- accelerator/conv.vhd
+++ accelerator/conv.vhd
@@ -31,7 +31,6 @@ entity conv is
 end conv;
 
 architecture Behavioral of conv is
-	type mult_array is array(y_size / dimensions_w(num_dimensions - 1) - 1 downto 0) of array_type(kernel_size * dimensions_x(num_dimensions - 2) - 1 downto 0)(data_width - 1 downto 0);
 	signal res, next_res : array_type(kernel_size * dimensions_x(num_dimensions - 2) - 1 downto 0)(data_width - 1 downto 0);
 
 	constant x_offs : integer := integer(ceil(real(kernel_shape(0) * dilation(0) - (dilation(0) - 1)) / 2)) - 1;
@@ -40,37 +39,60 @@ architecture Behavioral of conv is
 	type state_type is (START, DONE, SUM, MULT);
     signal state, next_state : state_type;
     
-    signal mult_a : mult_array;
+    signal mult_a : array_type(kernel_size * dimensions_x(num_dimensions - 2) - 1 downto 0)(data_width - 1 downto 0);
     signal mult_out : signed(data_width - 1 downto 0);
     signal sum_out : signed(data_width - 1 downto 0);
     
     signal next_out_array, out_array : array_type(y_size - 1 downto 0)(data_width - 1 downto 0);
     
-    signal next_index, index : integer := 0;
+    signal next_index_mult, index_mult : integer := 0;
     signal next_index_sum, index_sum : integer := 0;
+    signal next_index_xx, index_xx : integer := 0;
+    signal next_index_yy, index_yy : integer := 0;
+    signal next_index_xx_strided, index_xx_strided : integer := 0;
+    signal next_index_yy_strided, index_yy_strided : integer := 0;
     signal next_index_out_layer, index_out_layer : integer := 0;
     signal next_index_w_layer, index_w_layer : integer := 0;
+
+	signal x_edge, y_edge, layer_edge : boolean;
 begin
 	dim2 : if num_dimensions = 4 generate
 		--Assuming no layers to input image, might be wrong
 
 		-- Color channel loop
 		cl : for cc in 0 to dimensions_x(2) - 1 generate
-			-- Dimension loops
-			yl : for yy in 0 to dimensions_x(1) - 1 generate
-				xl : for xx in 0 to dimensions_x(0) - 1 generate
+			-- Kernel loop
+			yk : for yyk in 0 to kernel_shape(1) - 1 generate
+				xk : for xxk in 0 to kernel_shape(0) - 1 generate
+					-- If OOB, set element to 0
+					oob : if (xxk * dilation(0) - x_offs < 0) and (yyk * dilation(1) - y_offs < 0) generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when (index_xx_strided + xxk * dilation(1) - x_offs >= 0             ) and (index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) * stride(1) >= 0             ) else (others => '0');
 
-					-- Kernel loop
-                    yk : for yyk in 0 to kernel_shape(1) - 1 generate
-                        xk : for xxk in 0 to kernel_shape(0) - 1 generate
-                            -- If OOB, set element to 0
-                            oob : if (yy * stride(1) + yyk * dilation(1) - y_offs < 0) or (xx * stride(0) + xxk * dilation(0) - x_offs < 0) or (yy * stride(1) + yyk * dilation(1) - y_offs >= dimensions_x(1)) or (xx * stride(0) + xxk * dilation(0) - x_offs >= dimensions_x(0)) generate
-                                mult_a(xx + yy * dimensions_x(0))(xxk + yyk * kernel_shape(0)) <= (others => '0');
-                            else generate
-                                mult_a(xx + yy * dimensions_x(0))(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(xx * stride(0) + yy * stride(1) * dimensions_x(0) + (xxk * dilation(0) - x_offs) + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1));
-                            end generate;
-                        end generate;
-                    end generate;
+					elsif (xxk * dilation(0) - x_offs > 0) and (yyk * dilation(1) - y_offs < 0) generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when (index_xx_strided + xxk * dilation(1) - x_offs < dimensions_x(0)) and (index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) * stride(1) >= 0             ) else (others => '0');
+
+					elsif (xxk * dilation(0) - x_offs < 0) and (yyk * dilation(1) - y_offs > 0) generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when (index_xx_strided + xxk * dilation(1) - x_offs >= 0             ) and (index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) * stride(1) < dimensions_x(1) * dimensions_x(0) * stride(1)) else (others => '0');
+
+					elsif (xxk * dilation(0) - x_offs > 0) and (yyk * dilation(1) - y_offs > 0) generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when (index_xx_strided + xxk * dilation(1) - x_offs < dimensions_x(0)) and (index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) * stride(1) < dimensions_x(1) * dimensions_x(0) * stride(1)) else (others => '0');
+
+					elsif xxk * dilation(1) - x_offs < 0 generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + cc * dimensions_x(0) * dimensions_x(1)) when index_xx * stride(0) + xxk * dilation(0) - x_offs >= 0 else (others => '0');
+
+					elsif xxk * dilation(1) - x_offs > 0 generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (xxk * dilation(0) - x_offs) + cc * dimensions_x(0) * dimensions_x(1)) when index_xx * stride(0) + xxk * dilation(0) - x_offs < dimensions_x(0) else (others => '0');
+
+					elsif yyk * dilation(1) - y_offs < 0 generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when index_yy * stride(1) + yyk * dilation(1) - y_offs >= 0 else (others => '0');
+
+					elsif yyk * dilation(1) - y_offs > 0 generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + (yyk * dilation(1) - y_offs) * dimensions_x(0) + cc * dimensions_x(0) * dimensions_x(1)) when index_yy * stride(1) + yyk * dilation(1) - y_offs < dimensions_x(1) else (others => '0');
+
+					else generate
+						mult_a(xxk + yyk * kernel_shape(0) + cc * kernel_shape(0) * kernel_shape(1)) <= x(index_xx_strided + index_yy_strided + cc * dimensions_x(0) * dimensions_x(1));
+
+					end generate;
 				end generate;
 			end generate;
 		end generate;
@@ -80,8 +102,12 @@ begin
     begin
         valid_out <= '0';
         next_out_array <= out_array;
-        next_index <= 0;
+        next_index_mult <= 0;
         next_index_sum <= 0;
+		next_index_xx <= 0;
+		next_index_yy <= 0;
+		next_index_xx_strided <= 0;
+		next_index_yy_strided <= 0;
         next_state <= state;
         next_res <= res;
         next_index_out_layer <= index_out_layer;
@@ -89,9 +115,6 @@ begin
         
         case state is
             when START =>
-                next_index <= 0;
-                next_index_sum <= 0;
-                valid_out <= '0';
                 if valid_in then
                     next_state <= MULT;
                 else 
@@ -99,33 +122,57 @@ begin
                 end if;
             
             when MULT => 
-                next_index <= index + 1;
+                next_index_mult <= index_mult + 1;
                 next_index_sum <= index_sum;
-                next_res(index) <= mult_out;
-                if index = kernel_size * dimensions_x(num_dimensions - 2) - 1 then
+				next_index_xx <= index_xx;
+				next_index_xx_strided <= index_xx_strided;
+				next_index_yy <= index_yy;
+				next_index_yy_strided <= index_yy_strided;
+                next_res(index_mult) <= mult_out;
+                if index_mult = kernel_size * dimensions_x(num_dimensions - 2) - 1 then
                     next_state <= SUM;
-                    next_index <= 0;
+                    next_index_mult <= 0;
                 else
                     next_state <= MULT;
                 end if;
+
             when SUM => 
                 next_state <= MULT;
+
                 next_index_sum <= index_sum + 1;
-                next_index <= 0;
-                next_out_array(index_sum + index_out_layer) <= sum_out;
-                if index_sum = y_size / dimensions_w(num_dimensions - 1) - 1 then
+				next_index_xx <= index_xx + 1;
+				next_index_xx_strided <= index_xx_strided + stride(0);
+				next_index_yy <= index_yy;
+				next_index_yy_strided <= index_yy_strided;
+                next_index_mult <= 0;
+                next_out_array(index_xx + index_yy + index_out_layer) <= sum_out;
+                
+				if x_edge and y_edge and layer_edge then
+					next_state <= DONE;
+					next_index_out_layer <= 0;
+					next_index_w_layer <= 0;
+                    next_index_xx <= 0;
+                    next_index_xx_strided <= 0;
+                    next_index_yy <= 0;
+                    next_index_yy_strided <= 0;
+
+				elsif x_edge and y_edge then
                     next_index_out_layer <= index_out_layer + y_size / dimensions_w(num_dimensions - 1);
                     next_index_w_layer <= index_w_layer + w_size / dimensions_w(num_dimensions - 1);
-                    next_index_sum <= 0;
+                    next_index_xx <= 0;
+                    next_index_xx_strided <= 0;
+                    next_index_yy <= 0;
+                    next_index_yy_strided <= 0;
                     
-                    if index_out_layer = y_size - y_size / dimensions_w(num_dimensions - 1) then
-                        next_state <= DONE;
-                        next_index_out_layer <= 0;
-                        next_index_w_layer <= 0;
-                    end if;
+				elsif x_edge then
+					next_index_xx <= 0;
+                    next_index_xx_strided <= 0;
+					next_index_yy <= index_yy + dimensions_x(0);
+                    next_index_yy_strided <= index_yy_strided + dimensions_x(0) * stride(1);
+
                 end if;
+
             when DONE =>
-                next_index <= 0;
                 valid_out <= '1';
                 if not valid_in then
                     next_state <= START;
@@ -133,7 +180,6 @@ begin
                     next_state <= DONE;
                 end if;
         end case;
-    
     end process;
     
     process(clk)
@@ -141,16 +187,24 @@ begin
         if rising_edge(clk) then
             if rst then
                 state <= START;
-                index <= 0;
+                index_mult <= 0;
                 index_sum <= 0;
+				index_xx <= 0;
+				index_yy <= 0;
+				index_xx_strided <= 0;
+				index_yy_strided <= 0;
                 index_out_layer <= 0;
                 index_w_layer <= 0;
                 out_array <= (others => (others => '0'));
                 res <= (others => (others => '0'));
             else
                 state <= next_state;
-                index <= next_index;
+                index_mult <= next_index_mult;
                 index_sum <= next_index_sum;
+				index_xx <= next_index_xx;
+				index_yy <= next_index_yy;
+				index_xx_strided <= next_index_xx_strided;
+				index_yy_strided <= next_index_yy_strided;
                 index_out_layer <= next_index_out_layer;
                 index_w_layer <= next_index_w_layer;
                 out_array <= next_out_array;
@@ -158,6 +212,10 @@ begin
             end if;
         end if;
     end process;
+
+	x_edge <= index_xx = dimensions_x(0) - 1;
+	y_edge <= index_yy = dimensions_x(0) * (dimensions_x(1) - 1);
+	layer_edge <= index_out_layer = y_size - y_size / dimensions_w(num_dimensions - 1);
     
     y <= out_array;
     
@@ -166,8 +224,8 @@ begin
             data_width => 16
         )
         port map(
-            a => mult_a(index_sum)(index),
-            b => w(index + index_w_layer),
+            a => mult_a(index_mult),
+            b => w(index_mult + index_w_layer),
             res => mult_out
         );
         
