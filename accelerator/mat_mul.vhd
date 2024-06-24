12ecdf286e0f81a83b87f22e926c8ecccd0863c4
Jonas Jensen <jdudal11@gmail.com>
2024-06-24 16:05:10 +0200

    Updated mat mul to remove generator for loops and wires

--- accelerator/mat_mul.vhd
+++ accelerator/mat_mul.vhd
@@ -26,14 +26,11 @@ entity mat_mul is
 end mat_mul;
 
 architecture Behavioral of mat_mul is
-	type res_array is array(y_size - 1 downto 0) of array_type(a_dim(0) - 1 downto 0)(data_width - 1 downto 0);
-	signal next_res, res : array_type(a_dim(0) - 1 downto 0)(data_width - 1 downto 0);
+	signal res, next_res : array_type(a_dim(0) - 1 downto 0)(data_width - 1 downto 0);
 	
     type state_type is (START, DONE, SUM, MULT);
     signal state, next_state : state_type;
     
-    signal mult_a : res_array;
-    signal mult_b : res_array;
     signal mult_in_a : signed(data_width - 1 downto 0);
     signal mult_in_b : signed(data_width - 1 downto 0);
     signal mult_out : signed(data_width - 1 downto 0);
@@ -42,20 +39,14 @@ architecture Behavioral of mat_mul is
     
     signal next_out_array, out_array : array_type(y_size - 1 downto 0)(data_width - 1 downto 0);
     
-    signal next_index, index : integer;
-    signal next_index_sum, index_sum : integer;
+    signal next_index, index : integer := 0;
+    signal next_index_sum, index_sum : integer := 0;
+    signal next_index_x, index_x : integer := 0;
+    signal next_index_y, index_y : integer := 0;
+    signal next_index_ya, index_ya : integer := 0;
+    signal next_index_i, index_i : integer := 0;
+    signal next_index_ib, index_ib : integer := 0;
 begin
-	dim2 : if num_dimensions = 2 generate
-		yl : for yy in 0 to a_dim(1) - 1 generate
-			xl : for xx in 0 to b_dim(0) - 1 generate
-				il : for i in 0 to a_dim(0) - 1 generate
-				    mult_a(xx+yy*b_dim(0))(i) <= a(i + yy * a_dim(0));
-				    mult_b(xx+yy*b_dim(0))(i) <= b(xx + i * b_dim(0));
-				end generate;
-			end generate;
-		end generate;
-	end generate;
-	
 	process(all)
     begin
         valid_out <= '0';
@@ -64,49 +55,65 @@ begin
         mult_in_a <= (others => '0');
         mult_in_b <= (others => '0');
         next_index <= 0;
-        next_index_sum <= 0;
+        next_index_sum <= index_sum;
+        next_index_x <= index_x;
+        next_index_y <= index_y;
+		next_index_ya <= index_ya;
+        next_index_i <= 0;
+        next_index_ib <= 0;
         next_state <= state;
-        next_res <= res;
+		next_res <= res;
         
         case state is
             when START =>
-                next_index <= 0;
-                next_index_sum <= 0;
-                valid_out <= '0';
                 if valid_in then
                     next_state <= MULT;
-                else 
-                    next_state <= START;
                 end if;
             
             when MULT => 
                 next_index <= index + 1;
-                next_index_sum <= index_sum;
-                mult_in_a <= mult_a(index_sum)(index);
-                mult_in_b <= mult_b(index_sum)(index);
+				next_index_i <= index_i + 1;
+				next_index_ib <= index_ib + b_dim(0);
+
                 next_res(index) <= mult_out;
+
                 if index = a_dim(0) - 1 then
                     next_state <= SUM;
-                else
-                    next_state <= MULT;
+                    
+                    next_index <= 0;
+                    next_index_i <= 0;
+                    next_index_ib <= 0;
+
                 end if;
+
             when SUM => 
+                next_state <= MULT;
+
                 next_index_sum <= index_sum + 1;
-                next_index <= 0;
-                sum_in <= res;
+				next_index_x <= index_x + 1;
+
                 next_out_array(index_sum) <= sum_out;
+
                 if index_sum = y_size - 1 then
                     next_state <= DONE;
-                else
-                    next_state <= MULT;
+					
+					next_index_sum <= 0;
+					next_index_x <= 0;
+					next_index_y <= 0;
+					next_index_ya <= 0;
+
+				elsif index_x = b_dim(0) - 1 then
+					next_index_x <= 0;
+					next_index_y <= index_y + 1;
+					next_index_ya <= index_ya + a_dim(0);
+
                 end if;
+
             when DONE =>
-                next_index <= 0;
                 valid_out <= '1';
+
                 if not valid_in then
                     next_state <= START;
-                else
-                    next_state <= DONE;
                 end if;
         end case;
     
@@ -124,6 +131,11 @@ begin
             else
                 state <= next_state;
                 index <= next_index;
+                index_x <= next_index_x;
+                index_y <= next_index_y;
+                index_ya <= next_index_ya;
+                index_i <= next_index_i;
+                index_ib <= next_index_ib;
                 index_sum <= next_index_sum;
                 out_array <= next_out_array;
                 res <= next_res;
@@ -136,8 +148,8 @@ begin
     mul : entity work.fix_mul
         generic map(data_width => data_width)
         port map(
-            a => mult_in_a,
-            b => mult_in_b,
+            a => a(index_i + index_ya),
+            b => b(index_x + index_ib),
             res => mult_out
         );
         
@@ -147,7 +159,7 @@ begin
             num_inputs => a_dim(0)
         )
         port map(
-            a => sum_in,
+            a => res,
             c => sum_out
         );
 end Behavioral;
